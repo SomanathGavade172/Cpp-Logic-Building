749f60122272166a9a7b64fcf6f8ee9070a84e6e
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-03-04 18:56:20 +0530

    Day 12: Added Cpp basics programs

---
+++ Day_12/Program183.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application Name : Addition of Two Numbers
+//  Description      : This program performs addition of two integer values and displays the result on the console.
+//  Input            : None (Values are initialized inside the program)
+//  Output           : Displays the addition of two numbers
+//  Author           : Gavade Somanath Vilas
+//  Date             : 23/02/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iNo1 = 11, iNo2 = 21, iAns = 0;
+
+    iAns = iNo1 + iNo2;
+
+    cout << "Addition is : " << iAns << "\n";
+
+    return 0;
+}
