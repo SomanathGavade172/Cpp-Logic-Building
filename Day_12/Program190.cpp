7ff620261ab5ef2742a0ec7bd8424be9ba8b6dcd
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-04-15 19:51:22 +0530

    Day 12: Added Cpp basics programs

---
+++ Day_12/Program190.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application Name : Dynamic Array Input and Display Program
+//  Description      : This program accepts number of elements from user, stores them in dynamic array, and displays them.
+//  Input            : Integer values
+//  Output           : Displays elements of array
+//  Author           : Gavade Somanath Vilas
+//  Date             : 15/04/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iLength = 0, iCnt = 0;
+    int *Arr = NULL;
+
+    cout << "Enter number of elements that you want to store : \n";
+    cin >> iLength;
+
+    Arr = new int[iLength];
+
+    cout << "Enter " << iLength << " elements : \n";
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        cin >> Arr[iCnt];
+    }
+
+    cout << "Values from the Array are : \n";
+    
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        cout << Arr[iCnt] << "\n";
+    }
+
+    delete []Arr;
+
+    return 0;
+}
