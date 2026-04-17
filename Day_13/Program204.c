ec89538a29b7e132a4626bffc6aaa2151444b891
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-04-17 12:12:33 +0530

    Day 13: Added Bit ON/OFF programs in C++

---
+++ Day_13/Program204.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application Name : Bitwise Operations Program
+//  Description      : This program performs bitwise AND, OR and XOR operations on two integers.
+//  Input            : Two integers
+//  Output           : Displays result of bitwise operations
+//  Author           : Gavade Somanath Vilas
+//  Date             : 16/04/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iNo = 87;
+
+    printf("Number in Decimal format : %d\n", iNo);
+    printf("Number in Octal format : %o\n", iNo);
+    printf("Number in Hexadecimal format : %x\n", iNo);    
+
+    return 0;
+}
