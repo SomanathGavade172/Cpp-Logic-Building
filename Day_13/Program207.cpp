0359a48f1bafc19fb3d2f324da794e0aa64e5431
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-04-17 12:13:54 +0530

    Day 13: Bit ON/OFF programs in C++

---
+++ Day_13/Program207.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name    : CheckBit
+//  Description      : Checks whether the second bit of given number is ON or OFF using bitwise AND operation.
+//  Input            : Integer
+//  Output           : Returns true if second bit is ON, otherwise false
+//  Author           : Gavade Somanath Vilas
+//  Date             : 17/04/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+bool CheckBit(int iNo)
+{
+    int iMask = 0x2;
+    int iResult = 0;
+
+    iResult = iNo & iMask;
+
+    if(iResult == 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application Name    :   Check Second Bit Program
+//  Description         :   This application accepts a number from the user and checks whether its second bit is ON or OFF.
+//  
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    cout << "Enter a Number : \n";
+    cin >> iValue;
+
+    bRet = CheckBit(iValue);
+
+    if(bRet == true)
+    {
+        cout << "Bit is ON\n";
+    }
+    else
+    {
+        cout << "Bit is OFF\n";
+    }
+
+    return 0;
+}
